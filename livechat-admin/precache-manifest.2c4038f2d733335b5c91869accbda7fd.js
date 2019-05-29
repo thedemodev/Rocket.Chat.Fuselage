4ae5fb37b0e3d10a8c10d520e54e5b1999d34986
Guilherme Gazzo <guilherme.gazzo@rocket.chat>
2019-05-29 01:36:07 -0300

    Deploy Storybook to Github Pages [skip ci]

---
+++ livechat-admin/precache-manifest.2c4038f2d733335b5c91869accbda7fd.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186a57c62b62ea30e45893eb7a79accf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2818c49c59faa439757f.bundle.js"
+  },
+  {
+    "revision": "cffb71ac6b7cc9019879",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.2818c49c59faa439757f.bundle.js"
+  },
+  {
+    "url": "vendors~main.2818c49c59faa439757f.bundle.js"
+  }
+]);
